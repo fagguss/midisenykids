11a4dad1d554a9ab418f2d67c9ca94941761d772
fagguss <agustintavano7@gmail.com>
2022-07-15 19:39:59 -0300

    querys repo y empece crud en servicios

--- MiDisneyKids/src/main/java/com/alkemy/MiDisneyKids/servicios/PersonajeServicio.java
+++ MiDisneyKids/src/main/java/com/alkemy/MiDisneyKids/servicios/PersonajeServicio.java
@@ -1,14 +1,39 @@
-
 package com.alkemy.MiDisneyKids.servicios;
 
+import com.alkemy.MiDisneyKids.entidades.Pelicula;
+import com.alkemy.MiDisneyKids.entidades.Personaje;
 import com.alkemy.MiDisneyKids.repositorios.PersonajeRepositorio;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonajeServicio {
-    
+
     @Autowired
-    private PersonajeRepositorio personajeRepo; 
+    private PersonajeRepositorio personajeRepo;
+
+//-----------------------CRUD------------------
     
+    public void crear(String id, String nombre, String edad, Double peso, String historia, List<Pelicula> peliculas) {
+
+        Personaje personaje = new Personaje();
+
+        personaje.setId(id);
+        personaje.setNombre(nombre);
+        personaje.setEdad(edad);
+        personaje.setPeso(peso);
+        personaje.setHistoria(historia);
+
+        personajeRepo.save(personaje);
+
+    }
+    
+    public void eliminar(String id, String nombre){
+        
+        personajeRepo.buscarPersonajePorId(id); 
+        
+        
+        personajeRepo.delete(personaje);
+    }
 }
